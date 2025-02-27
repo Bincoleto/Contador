73385f40660c7947f6ce599002f50d42eb87af13
Marcio Bincoleto Junior <65255138+Bincoleto@users.noreply.github.com>
2025-02-27 19:23:31 -0300

    Nova correção
    Feito correção no JS para salvar o ultimo resultado.

--- contador.js
+++ contador.js
@@ -5,7 +5,13 @@ const clickSomar = document.querySelector('#somar');
 function somar(){
     const contador = document.querySelector('.numero-contador');
     const total = Number(contador.innerText) + 1;
+    salva(Number(contador.innerText))
     contador.innerText = total;
+}
+
+function salva(num){
+    const contador = document.querySelector('.num-ant');
+    contador.innerText = num;
 }
 
 if(clickSomar){
@@ -19,7 +25,7 @@ const clickRemover = document.querySelector("#remover");
 function subtrair(){
     const contador = document.querySelector('.numero-contador');
     const totalAtual = Number(contador.innerText);
-
+    salva( totalAtual)
     if(totalAtual > 0){
         const total = totalAtual - 1
         contador.innerText = total;
@@ -34,6 +40,7 @@ const clickZerar = document.querySelector('#zerar');
 
 function zerarContador(){
     const contador = document.querySelector('.numero-contador');
+    salva( Number(contador.innerText))
     contador.innerText = 0;
 }
 
